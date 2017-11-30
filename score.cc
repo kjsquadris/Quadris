4e3fd16b75e74781d3b5ac4a7b5bf6faf9faa950
Jennifer Chan <jenniferrxchan@gmail.com>
2017-11-30 11:52:58 -0500

    Add functions to calculate, update, restart, reset, retrieve scores

--- score.cc
+++ score.cc
@@ -1,1 +1,49 @@
+#include <cmath>
 #include "score.h"
+
+using namespace std;
+
+// calculate score when a block is cleared
+void Score::blockClear(Block b) {
+  int lvl = b.getLevel();
+  int bscore = pow(lvl + 1, 2);
+  setScore(bscore); // update the current score
+}
+
+// calculate score when line(s) are cleared
+int Score::rowClear(int rowsCleared, int currLevel) {
+  int rscore = pow(currLevel + rowsCleared, 2);
+  setScore(rscore); // update the current score
+}
+
+// add to the current score (and maybe high score)
+void Score::updateScore(int x) {
+  currentScore += x;
+
+  // check if the current score has exceeded high score
+  // if so, then update highscore
+  if (currentScore > highScore) {
+    highScore = currentScore;
+  }
+}
+
+// restart the game and set current score to 0
+void Score::restart() {
+  currentScore = 0;
+}
+
+// reset the scores
+void Score::reset() {
+  currentScore = 0;
+  highScore = 0;
+}
+
+// retrieve the current score
+int Score::getCurrentScore() {
+  return currentScore;
+}
+
+// retrieve the high score
+int Score::getHiscore() {
+  return highScore;
+}
--- score.h
+++ score.h
@@ -5,9 +5,13 @@ class Score {
   int currentScore = 0;
   int highScore = 0;
 public:
-  void setScore();
+  int blockClear();
+  int rowClear();
+  void updateScore();
+  void restart();
+  void reset();
   int getCurrentScore();
-  int getHiscore();
+  int getHiScore();
 };
 
 #endif

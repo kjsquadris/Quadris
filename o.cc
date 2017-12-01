2c84691455e530f23a714b8b94ca9c6f4628c543
Jennifer Chan <jenniferrxchan@gmail.com>
2017-11-30 23:23:39 -0500

    Complete O block class

--- o.cc
+++ o.cc
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-O::~O() {}
-
-void O::init(Board* b, int lvl) {
-  // (0,3), (1,3), (0,4), (1,4)
-  Cell* c1 = b->getCell(0,3);
+void O::init(Grid *g, int lvl) {
+  Cell* c1 = &(g->getCell(3,0));
   cells.emplace_back(c1);
-  Cell* c2 = b->getCell(1,3);
+  Cell* c2 = &(g->getCell(3,1));
   cells.emplace_back(c2);
-  Cell* c3 = b->getCell(0,4);
+  Cell* c3 = &(g->getCell(4,0));
   cells.emplace_back(c3);
-  Cell* c4 = b->getCell(1,4);
+  Cell* c4 = &(g->getCell(4,1));
   cells.emplace_back(c4);
   level = lvl;
+  type = "oBlock";
+  set();
+  draw();
 }
 
-void O::rotateCW() {
-}
+bool isValidRotate() { return true; }
 
-void O::rotateCCW() {
-}
+void O::rotateCW() {}
+
+void O::rotateCCW() {}
--- o.h
+++ o.h
@@ -1,17 +1,21 @@
 #ifndef O_H
 #define O_H
 #include <vector>
+#include <memory>
+#include <string>
 #include "block.h"
 
 class Cell;
 
 class O : public Block {
-  vector<Cell*> cells;
+  // std::vector<std::unique_ptr<Cell>> cells;
+  std::vector<Cell*> cells;
   int counter = 0;
   int level;
+  std::string type;
 public:
-  ~O();
   void init() override;
+  bool isValidRotate() override;
   void rotateCW() override;
   void rotateCCW() override;
 };

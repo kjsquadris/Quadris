cab6e7dd94a5226327a77df9b73bbb101d693c44
Jennifer Chan <jenniferrxchan@gmail.com>
2017-11-30 21:25:37 -0500

    Complete declaration and definition of abstract Block class and methods

--- block.cc
+++ block.cc
@@ -1,82 +1,209 @@
 #include <string>
+#include <vector>
 #include <memory>
 #include "block.h"
 #include "cell.h"
 
 using namespace std;
-Block::~Block() {}
 
+// destructor
+Block::~Block() {
+  // change all cell ptrs in block to point at nullptr
+  // before popping them off the vector
+  for (int i = 0; i < 4; ++i) {
+    cells.rbegin()[0] = nullptr;
+    cells.pop_back();
+  }
+  g = nullptr; // set the grid pointer to nullptr
+}
+
+// set g pointer to point at the grid
+void Block::setGrid(unique_ptr<Grid> gameGrid) {
+  g = gameGrid;
+}
+
+// returns the level the block was created in
 int Block::getLevel() {
   return level;
 }
 
-vector<Cell*> Block::getCells() {
+// returns the cells of the block
+vector<unique_ptr<Cell>> Block::getCells() {
   return cells;
 }
 
+// checks if the block's cells are all cleared
 bool Block::isEmpty() {
   if (cells.empty()) return true;
   return false;
 }
 
-bool Block::isValidMove(string dir) {
-  if (dir == "left") {
-    // check if the pointer to the left is isOccupied
+// create a vector with all the coordinates of the current cells of the block
+vector<Coordinate> Block::createCoords() {
+  vector<Coordinate> coords;
 
-  } else if (dir == "right") {
+  for (int i = 0; i < 4; ++i) {
+    Coordinate c;
+    c.x = cells[i]->x;
+    c.y = cells[i]->y;
+    coords.emplace_back(c);
+  }
+  return coords;
+}
 
-  } else if (dir == "down") {
+// check whether a cell is inside the block
+bool Block::inBlock(int x, int y) {
+  vector<Coordinate> coords = createCoords(); // hold the coordinates of curr cells
+  bool in = false;
 
-  } else if (dir == "cw") {
+  for (int i = 0; i < 4; ++i) {
+    if ((coords[i].x == x) && (coords[i].y == y)) {
+      in = true;
+      break;
+    }
+  }
+  return in;
+}
 
-  } else if (dir == "ccw") {
+// check if the cell the block is trying to shift into is occupied or not
+// (only checks for left, right, down shift)
+bool Block::isValidShift(string dir) {
+  vector<Coordinate> coords = createCoords(); // hold the coordinates of curr cells
+  vector<vector<Cell>> gridcells = g->getCells(); // hold the cells of the grid
 
+  bool valid = true;
+  if ((dir != "left") && (dir != "right") && (dir != "down") {
+    return false;
+  } else {
+    for (int i = 0; i < 4; ++i) {
+      int x = coords[i].row;
+      int y = coords[i].col;
+      if (dir == "left") {
+        x -= 1;
+      } else if (dir == "right") {
+        x += 1;
+      } else if (dir == "down") {
+        y += 1;
+      }
+
+      if ((x < 0) || (x > 10) || (y > 17)) { // check if cell is not on board
+        valid = false;
+        break;
+      } else if (gridcells[x][y].isOccupied()) { // check if cell is occupied
+        if (inBlock(x,y)) { // check if occupied cell is part of the same block
+          continue;
+        } else { // if not then invalid move
+          valid = false;
+          break;
+        }
+      } else { // unoccupied
+        continue;
+      }
+    }
+  }
+  return valid;
+}
+
+// move the block to the left
+void Block::moveLeft() {
+  if (isValidShift("left")) {
+    unset(); // unset the current cells of the block
+
+    // reassign the cells of the block
+    vector<vector<Cell>> gridcells = g->getCells(); // holds the grid's cells
+
+    // move to the left
+    for (int i = 0; i < 4; ++i) {
+      int x = cells[i]->x - 1;
+      int y = cells[i]->y;
+      cells[i].reset(new Cell(gridcells[x][y]));
+    }
+    set(type);
+
+    draw();
+  }
+}
+
+// move the block to the right
+void Block::moveRight() {
+  if (isValidShift("right")) {
+    unset();
+
+    // reassign the cells of the block
+    vector<vector<Cell>> gridcells = g->getCells(); // holds the grid's cells
+
+    // move to the right
+    for (int i = 0; i < 4; ++i) {
+      int x = cells[i]->x + 1;
+      int y = cells[i]->y;
+      cells[i].reset(new Cell(gridcells[x][y]));
+    }
+    set(type);
+
+    draw();
+  }
+}
+
+// move the block down
+bool Block::moveDown(string cmd) {
+  if (isValidShift("down")) {
+    unset();
+
+    // reassign the cells of the block
+    vector<vector<Cell>> gridcells = g->getCells(); // holds the grid's cells
+
+    // move down
+    for (int i = 0; i < 4; ++i) {
+      int x = cells[i]->x;
+      int y = cells[i]->y + 1;
+      cells[i].reset(new Cell(gridcells[x][y]));
+    }
+    set(type);
+
+    // draw right away if it's just a down move
+    if (cmd == "down") {
+      draw();
+    } // otherwise don't draw until drop calls draw
+
+    return true;
   } else {
     return false;
   }
 }
 
-void Block::moveLeft() {
-  if (isValidMove("left")) {
-    unset();
-    // move to the left
-    draw();
-  }
-}
-
-void Block::moveRight() {
-  if (isValidMove("right")) {
-    unset();
-    // move to the right;
-    draw();
-  }
-}
-
-void Block::moveDown() {
-  if (isValidMove("down")) {
-    unset();
-    // move to the down;
-    draw();
-  }
-}
-
+// move the block as far down as it can go without overtaking another cell
 void Block::drop() {
-  // drop it
+  while (moveDown("drop")) {
+    continue;
+  }
   draw();
 }
 
+// notify the cells that they've moved
 void Block::draw() {
   for (int i = 0; i < 4; ++i) {
     cells[i]->draw();
   }
 }
 
+// set the cells of block
+void Block::set() {
+  for (int i = 0; i < 4; ++i) {
+    cells[i]->setBlockType(type);
+  }
+}
+
+// unset the current cells of the block
 void Block::unset() {
   for (int i = 0; i < 4; ++i) {
-    cells[i]->unset();
+    cells[i]->unsetBlockType();
   }
 }
 
 void Block::updateCounter() {
   counter++;
+}
+
+string Block::getString() {
+  return type;
 }
--- block.h
+++ block.h
@@ -1,28 +1,41 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 #include <vector>
+#include <memory>
+#include <string>
+#include "coordinate.h"
 
 class Block {
-  std::vector<Cell*> cells;
+  std::vector<std::unique_ptr<Cell>> cells;
+  std::unique_ptr<Grid> g; // points to the grid
   int counter = 0;
   int level;
+  std::string type;
 public:
-  virtual ~Block() = 0;
+  // pure virtual functions to be overridden by derived classes
   virtual void init() = 0;
-  virtual int getLevel();
-  virtual std::vector<Cell*> getCells();
-  //virtual void setCoord() = 0;
-  virtual bool isEmpty();
-  virtual bool isValidMove();  
+  virtual bool isValidRotate() = 0;
   virtual void rotateCW() = 0;
   virtual void rotateCCW() = 0;
+
+  // default methods for all blocks
+  virtual ~Block();
+  virtual void setGrid(std::unique_ptr<Grid>);
+  virtual int getLevel();
+  virtual std::vector<std::unique_ptr<Cell>> getCells();
+  virtual bool isEmpty();
+  virtual std::vector<Coordinate> createCoords();
+  virtual bool inBlock(int, int);
+  virtual bool isValidShift(std::string dir);  
   virtual void moveLeft();
   virtual void moveRight();
-  virtual void moveDown();
+  virtual void moveDown(std::string cmd = "down");
   virtual void drop();
   virtual void draw();
+  virtual void set(std::string);
   virtual void unset();
   virtual void updateCounter();
+  virtual std::string getType();
 };
 
 #endif
